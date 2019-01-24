20e564133096dab3d1bccae634900fc9d196c5c2
Martti <marttij@kth.se>
2019-01-24 17:02:32 +0100

    cleaned up the python folder

--- Python/codetestnr1.py
+++
@@ -1,9 +0,0 @@
-#Code for testing nr1
-
-for i in range(10):
-    print('kexkexkex')
-
-
-
-RL = 30
-rl = 222
--- Python/test_for_mspac_ver1.py
+++
@@ -1,6 +0,0 @@
-#gabetest for mspacman nr1
-
-import gym
-env = gym.make('MsPacman-v0')
-env.reset()
-env.render()
