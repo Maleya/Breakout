a0b19ecd66a4b32ef00f9594ca1284bbac2913c7
Gabriel Andersson <Gabriel@Gabriels-MBP.lan>
2019-01-30 14:11:25 +0100

    adding mean reward for random agent

--- Python/random_agent.py
+++ Python/random_agent.py
@@ -1,16 +1,24 @@
 import gym
-
+import numpy as np
 # Create a breakout environment
 env = gym.make('MsPacman-v0')
-# Reset it, returns the starting frame
-frame = env.reset()
-# Render
-env.render()
 
-is_done = False
-while not is_done:
-  # Perform a random action, returns the new frame, reward and whether the game is over
-  frame, reward, is_done, _ = env.step(env.action_space.sample())
-  # Render
-  env.render()
+v_reward = []
+for i in range(30):
+    # Reset it, returns the starting frame
+    frame = env.reset()
+    # Render
+    #env.render()
+    is_done = False
+    tot_reward = 0
+    while not is_done:
+
+      # Perform a random action, returns the new frame, reward and whether the game is over
+      frame, reward, is_done, _ = env.step(env.action_space.sample())
+      tot_reward += reward
+      # Render
+      #env.render()
+    v_reward.append(tot_reward)
+
 env.close()
+print(np.mean(v_reward))
