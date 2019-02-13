e9305650fe80451a28b85c49e60cd8aa34345b04
Gabriel Andersson <Gabriel@n175-p231.eduroam.kth.se>
2019-02-13 16:06:48 +0100

    Adding visability of reward and bugfix

--- Python/DQN_agent_ver1.py
+++ Python/DQN_agent_ver1.py
@@ -4,7 +4,7 @@ import numpy as np
 import random as rnd
 from collections import deque
 from NN_ver1 import DQN_net
-
+env = gym.make('MsPacman-v0')
 
 class DQN_Agent:
     def __init__(self, state_size, action_size,
@@ -19,7 +19,9 @@ class DQN_Agent:
         self.discount_factor = discount_factor  # Discount factor of the MDP (gamma)
         self.learning_rate = learning_rate  # Learning rate (alpha)
         self.batch_size = batch_size
-
+        #For reseting target network
+        self.learning_count = 0
+        self.learning_count_max = 1000
         #Replay Memory for bootstrapping
         self.memory = deque(maxlen=2000)
 
@@ -57,6 +59,12 @@ class DQN_Agent:
 
     def sample_experience(self):
         return rnd.sample(self.memory, self.batch_size)
+
+    def reset_target_network(self):
+        """
+        Updates the target DQN with the current weights of the main DQN.
+        """
+        self.target_network.model.set_weights(self.network.model.get_weights())
 
 
 #TEST CODE
--- Python/DQN_algorithm.py
+++ Python/DQN_algorithm.py
@@ -1,6 +1,6 @@
-import keras
-from keras.models import Sequential
-from keras.layers import Conv2D, Flatten, Dense
+#import keras
+#from keras.models import Sequential
+#from keras.layers import Conv2D, Flatten, Dense
 import gym
 import numpy as np
 import random as rnd
@@ -8,12 +8,15 @@ from collections import deque
 from NN_ver1 import DQN_net
 from pre_process import pre_process
 from DQN_agent_ver1 import DQN_Agent
-
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
 
 env = gym.make('MsPacman-v0')
 
 def episode(agent):
     '''Docstring'''
+    Return = 0
     frame = env.reset()
     frame = pre_process(frame)
     history_stack = deque(maxlen = 4)
@@ -27,21 +30,24 @@ def episode(agent):
         #counter += 1
         action  = agent.get_action(state)
         new_frame, reward, is_done, _ = env.step(action)
+        Return += reward
         #Procecessing images to 4D tensor for the conv_2D input
         new_frame = pre_process(new_frame)
         history_stack.append(new_frame)
         new_state = np.stack((history_stack[0], history_stack[1], history_stack[2], history_stack[3]), axis=-1)
         new_state = np.expand_dims(new_state, axis=0)
         agent.add_experience(state, action, reward, new_state, is_done)
-        '''
+
+        #Network wights update:
         if len(agent.memory) >= agent.batch_size:
             experience_batch = agent.sample_experience()
             agent.network.train(experience_batch, agent.target_network)
-            '''
+            agent.learning_count += 1
+            if agent.learning_count % agent.learning_count_max == 0:
+                agent.reset_target_network()
         state = new_state
-    if len(agent.memory) >= agent.batch_size:
-        experience_batch = agent.sample_experience()
-        agent.network.train(experience_batch, agent.target_network)
+
+    return Return
 #counter = 0
 def train(num_episodes):
     '''Docstring'''
@@ -53,15 +59,21 @@ def train(num_episodes):
     print('state size =',state_size)
     action_size = env.action_space.n
     DQNAgent = DQN_Agent(state_size, action_size,
-                         batch_size = 32,
+                         batch_size = 500,
                          discount_factor = 0.95,
                          learning_rate = 0.00025,
-                         epsilon = 0)
-
+                         epsilon = 0.2)
+    Return_history = []
     for eps in range(num_episodes):
-        episode(DQNAgent)
+        Return = episode(DQNAgent)
+        Return_history.append(Return)
+        print('Return for episode:',eps,'is:',Return)
+    return Return_history
 
 
 if __name__ == "__main__":
-    num_episodes = 1
-    train(num_episodes)
+    num_episodes = 10
+    Return_history = train(num_episodes)
+    episodes_v = [i for i in range(num_episodes)]
+    plt.plot(episodes_v, Return_history, '.')
+    plt.show()
