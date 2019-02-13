94a3e362f0f8f94b786ab623d3f3ebd084d8de90
Gabriel Andersson <Gabriel@n175-p231.eduroam.kth.se>
2019-02-13 09:43:41 +0100

    Continued to work on algorithm 1 from atari paper, still problem with #frames for network input

--- Python/DQN_algorithm.py
+++ Python/DQN_algorithm.py
@@ -14,27 +14,42 @@ from DQN_agent_ver1 import DQN_Agent
 def episode(agent):
     frame = env.reset()
     frame = pre_process(frame)
+    history_stack = deque(maxlen = 4)
+    for i range(4):
+        history_stack.append(frame)
+    #new_state = np.stack((frame, frame, frame, frame), axis=-1)
     is_done = False
     while not is_done:
-      # Perform a random action, returns the new frame, reward and whether the game is over
-      new_frame, reward, is_done, _ = env.step(action)
-      new_frame = pre_process(new_frame)
+        #counter += 1
+        action  = agent.get_action(frame)
+        new_frame, reward, is_done, _ = env.step(action)
+        new_frame = pre_process(new_frame)
+        new_state[-1] = new_frame
+        agent.add_experience(state, action, reward, new_state, done)
+        if len(agent.memory) >= agent.batch_size:
+            experience_batch = agent.sample_experience()
+            agent.network.train(experience_batch, agent.target_network)
 
+        frame = new_frame
 
-
-
+env = gym.make('MsPacman-v0')
+#counter = 0
 def train(num_episodes):
-    env = gym.make('MsPacman-v0')
     frame = env.reset()
     frame = pre_process(frame)
     state = np.stack((frame, frame, frame, frame), axis=-1)
+    state = frame
     state_size = state.shape
     print(state_size)
     action_size = env.action_space.n
-    DQNAgent = DQN_Agent(state_size, action_size)
+    DQNAgent = DQN_Agent(state_size, action_size,
+                         batch_size = 32,
+                         discount_factor = 0.95,
+                         learning_rate = 0.00025,
+                         epsilon = 0.1)
 
-    for episode in range(num_episodes):
-        pass
+    for eps in range(num_episodes):
+        episode(DQNAgent)
 
 
 if __name__ == "__main__":
