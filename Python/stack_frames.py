5906bacefdd260c79359914621d692f86a0257fb
Martti <marttij@kth.se>
2019-02-21 14:46:39 +0100

    fixed docstrings

--- Python/stack_frames.py
+++ Python/stack_frames.py
@@ -5,20 +5,17 @@ import numpy as np
 
 class stack_frames:
     '''
-    Functions takes a preprocessed frame as input.
+    Functions takes a preprocessed frame as input. Basically a Deque with more functionality. 
     '''
-    def __init__(self, stack_size = 4):
+    def __init__(self, stack_size=4):
 
         self.stack_size = stack_size
-        self.frame_stack = deque(maxlen = stack_size)
-
-    def create_stack(self,initial_frame):
+        self.frame_stack = deque(maxlen=stack_size)
+        
+    def create_stack(self, initial_frame):
         '''
-        To be implemented in the begining of a new episode.
         Creates a full stack of the initial frame only and stacks them
-        to create a state, which is expanded due to formatting issues..
-
-        Output hace the correct dimensions to be fed into the NN.
+        to create a state that our NN can read. 
         '''
         for i in range(self.stack_size):
             self.frame_stack.append(initial_frame)
@@ -28,9 +25,8 @@ class stack_frames:
 
     def get_new_state(self,new_frame):
         '''
-        Add a new state at the end of the stack.
-
-        Output has the correct dimensions to be fed into the NN.
+        Adds a new frame to stack and outputs correct (np.stacked) dim
+        to be fed to NN.
         '''
         self.frame_stack.append(new_frame)
         new_state = np.stack((elem for elem in self.frame_stack),axis=-1)
