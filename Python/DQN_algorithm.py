787b6d98777d6bc1c3d2fdb46bd861c84f9f2ae3
Martti <marttij@kth.se>
2019-02-25 18:05:04 +0100

    updated weights 10000 step run

--- Python/DQN_algorithm.py
+++ Python/DQN_algorithm.py
@@ -129,7 +129,7 @@ def run_training(num_learning_iterations):
 
 if __name__ == "__main__":
     start_time = time.time()
-    num_learning_iterations = 100
+    num_learning_iterations = 10000
     points_history, DQNAgent = run_training(num_learning_iterations)
     env.close()
 
