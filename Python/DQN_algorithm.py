be66aefbdb1558468461490137c1f02a636f7f20
Martti <marttij@kth.se>
2019-02-25 18:40:58 +0100

    ran a 20000 learning set

--- Python/DQN_algorithm.py
+++ Python/DQN_algorithm.py
@@ -129,7 +129,7 @@ def run_training(num_learning_iterations):
 
 if __name__ == "__main__":
     start_time = time.time()
-    num_learning_iterations = 10000
+    num_learning_iterations = 20000
     points_history, DQNAgent = run_training(num_learning_iterations)
     env.close()
 
