e6fd79f32845ea46ea59cb3c5b19a7ee0257f46b
maleya <marttiyap@gmail.com>
2019-03-14 23:20:55 +0100

    changes

--- Python/arraysize_test.py
+++ Python/arraysize_test.py
@@ -1,10 +1,21 @@
 
-from tempfile import TemporaryFile
+#from tempfile import TemporaryFile
 import numpy as np
 import pickle, os, time
 import gzip, lzma, bz2, h5py
 
-outfile = TemporaryFile()
-x = np.zeros([84,84,1,1,1,10**6//4],dtype=np.int8)
-#np.save(outfile, x)
-print(f"a Numpy array of size {x.shape} would be {x.nbytes/10**9} Gigabytes")
+#outfile = TemporaryFile()
+x = np.zeros([84,84,1,1,1,10**6//2],dtype=np.int8)
+r = np.random.randint(5, size=(84,84,1,1,1,10**6//2))
+print(f"a Numpy array of size {r.shape} would be {r.nbytes/10**9} Gigabytes")
+
+
+
+# normal save
+np.save('outfile', r)
+
+
+# gzip 
+f = gzip.GzipFile('file.npy.gz', "w")
+np.save(f, r)
+f.close()
