681e7b9803ec48739ecf49db3e4f22de64a36a25
Martti <marttij@kth.se>
2019-03-15 12:27:15 +0100

    renamed files for clairity

---
+++ Python/preprocess.py
@@ -0,0 +1,27 @@
+"""
+Preprocessing function that expects an np.array with shape (210, 160, 3)
+"""
+
+from PIL import Image
+import numpy as np
+
+
+def preprocess(im):
+    im = Image.fromarray(im)
+    im = im.convert('L')
+    im = im.crop((8, 31, 152, 210))  # somewhat good settings for breakout
+    im = im.resize((84, 84))
+    # im.show()
+    return(np.array(im))
+
+
+if __name__ == "__main__":
+    # generate a realistic frame
+    import gym
+    env = gym.make('Breakout-v0')
+    frame = env.reset()
+    frame, reward, done, _, = env.step(env.action_space.sample())
+    env.close()
+    output = preprocess(frame)
+    print(output)
+    print(f"input shape: {frame.shape}, output shape: {output.shape}")
